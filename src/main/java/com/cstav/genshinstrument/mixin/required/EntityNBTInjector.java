e7d23aa427c1ca3efcc4bf40d3b511002987d6bf
StavWasPlayZ <stavsolomon101@gmail.com>
2024-08-28 13:20:15 +0300

    Make getPersistentData mixin unique

--- src/main/java/com/cstav/genshinstrument/mixin/required/EntityNBTInjector.java
+++ src/main/java/com/cstav/genshinstrument/mixin/required/EntityNBTInjector.java
@@ -1,5 +1,9 @@
 package com.cstav.genshinstrument.mixin.required;
 
+import com.cstav.genshinstrument.mixin.util.IEntityModData;
+import com.cstav.genshinstrument.mixin.util.MixinConstants;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -7,20 +11,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import com.cstav.genshinstrument.mixin.util.IEntityModData;
-import com.cstav.genshinstrument.mixin.util.MixinConstants;
-
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.entity.Entity;
-
 @Mixin(Entity.class)
 public abstract class EntityNBTInjector implements IEntityModData {
     
     @Unique
     private CompoundTag persistentData;
 
+    @Unique
     @Override
-    public CompoundTag getPersistentData() {
+    public CompoundTag genshinstrument$getPersistentData() {
         if (persistentData == null)
             persistentData = new CompoundTag();
         
